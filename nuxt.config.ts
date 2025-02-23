b9c80df4268656916f2b8066729b49816e1f888b
hansnaa3193378 <hansaa3193378@gmail.com>
2025-02-23 12:23:03 +0200

    add package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,14 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
+
   modules: [
     "@pinia/nuxt",
     "@nuxtjs/i18n",
     "vuetify-nuxt-module",
     "@vueuse/nuxt",
     "maz-ui/nuxt",
+    "vue3-carousel-nuxt",
   ],
 
   i18n: {
